48703a77b5aae0b648f2629472b7981a5c4f9d21
michaelnhw <michaelnhw@gmail.com>
2014-01-24 17:14:40 -0800

    initial

---
+++ sfuHAck_eureka/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using sfuHAck_eureka.Resources;
+
+namespace sfuHAck_eureka
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
